5f543c2bd2c9e70944406bef36515891d39c99d1
ChenerZHANG1025 <ssycz8@nottingham.edu.cn>
2025-04-14 21:50:44 +0800

    task(a) (b) (c)

--- matlab-cw2/Chener_ZHANG_20614880.m
+++ matlab-cw2/Chener_ZHANG_20614880.m
@@ -8,14 +8,35 @@ clear all
 clear
 a = arduino("/dev/cu.usbserial-10","Uno");
 for i = 1:10  
-    writeDigitalPin(a, 'D7', 1);  
+    writeDigitalPin(a,'D7',1);  
     pause(0.5);  
-    writeDigitalPin(a, 'D7', 0);  
+    writeDigitalPin(a,'D7',0);  
     pause(0.5);  
 end
 %% TASK 1 - READ TEMPERATURE DATA, PLOT, AND WRITE TO A LOG FILE [20 MARKS]
 
 % Insert answers here
+clear
+a = arduino("/dev/cu.usbserial-10","Uno");
+V_0c = 0.5;
+T_c = 0.01;
+duration = 600;
+sampling_interval = 1;
+time = 1:duration;
+for t = 1:duration
+    A0_voltage(t) = readVoltage(a,'A0');
+    temperature(t) = (A0_voltage(t)-V_0c)/T_c;
+end
+min_temp = min(temperature);
+max_temp = max(temperature);
+avg_temp = mean(temperature);
+figure;
+plot(time,temperature);
+xlabel('Time (s)');
+xlim([0 duration]);
+ylabel('Temperature (Celsius)');
+grid on;
+
 %% TASK 2 - LED TEMPERATURE MONITORING DEVICE IMPLEMENTATION [25 MARKS]
 
 % Insert answers here
