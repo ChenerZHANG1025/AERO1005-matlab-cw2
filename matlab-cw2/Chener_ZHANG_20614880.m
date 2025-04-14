7d720ea13ee02b27592f833a5cd1c1c797fc8293
ChenerZHANG1025 <ssycz8@nottingham.edu.cn>
2025-04-14 14:03:43 +0800

    pre task

--- matlab-cw2/Chener_ZHANG_20614880.m
+++ matlab-cw2/Chener_ZHANG_20614880.m
@@ -1,15 +1,21 @@
 % Chener ZHANG
 % ssycz8@nottingham.edu.cn
 
-
+clear all
 %% PRELIMINARY TASK - ARDUINO AND GIT INSTALLATION [10 MARKS]
 
 % Insert answers here
-a=1+2;
+clear
+a = arduino("/dev/cu.usbserial-10","Uno");
+for i = 1:10  
+    writeDigitalPin(a, 'D7', 1);  
+    pause(0.5);  
+    writeDigitalPin(a, 'D7', 0);  
+    pause(0.5);  
+end
 %% TASK 1 - READ TEMPERATURE DATA, PLOT, AND WRITE TO A LOG FILE [20 MARKS]
 
 % Insert answers here
-
 %% TASK 2 - LED TEMPERATURE MONITORING DEVICE IMPLEMENTATION [25 MARKS]
 
 % Insert answers here
