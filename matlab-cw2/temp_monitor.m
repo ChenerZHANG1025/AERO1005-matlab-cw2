a87f65dcb8c4480057ec6a25df85973fcad91f71
ChenerZHANG1025 <ssycz8@nottingham.edu.cn>
2025-04-22 22:29:31 +0800

    comment on temp_monitor function

--- matlab-cw2/temp_monitor.m
+++ matlab-cw2/temp_monitor.m
@@ -25,58 +25,65 @@ xlim([0 30])
 ylim([10 40]); % limit the y axis from 10 t0 40
 hold on; % keep the figure drawing
 
-n = 30;  % 30秒数据缓冲区
-time_data = NaN(1, n);  % 初始化为NaN（避免零值干扰）
-temp_data = NaN(1, n);
-idx = 1;  % 当前写入位置
+n = 30;  % set 30 data points for each group of data
+time_data = NaN(1, n); % set an empty array with the length of the whole duration for collecting time data
+temp_data = NaN(1, n); % set an empty array with the length of the whole duration for collecting temperature data
+idx = 1; % let the data collect from number 1, and make an index for the data collected from the arduino
  % set an empty array with the length of the whole duration for collecting time data
  % set an empty array with the length of the whole duration for collecting temperature data
  % let the data collect from number 1, and make an index for the data collected from the arduino
 
-writeDigitalPin(a, 'D3', 0);
-writeDigitalPin(a, 'D5', 0);
-writeDigitalPin(a, 'D7', 0);
+% Make the LED  ccxdcxlight off at the beginning
+writeDigitalPin(a, 'D3', 0); % D3 connects with green LED
+writeDigitalPin(a, 'D5', 0); % D5 connects with yellow LED
+writeDigitalPin(a, 'D7', 0); % D7 connects with red LED
 
 tic; % start the timer
 while true % determine whether the elapsed time is greater than 600 seconds
     time = toc; % set the variable
-    A0_voltage = readVoltage(a, 'A0');
-    temp = (A0_voltage - 0.5) / 0.01;
+    A0_voltage = readVoltage(a, 'A0'); % record the voltage of the temperature sensor which is connect to the analogue pin (A0)
+    temp = (A0_voltage - 0.5) / 0.01;  % calculate the temperature based on the voltage
+    % set the limit of the x axis to adapt the change of the time
+    % when time larger than 30 s, change the limitation of the x axis from the 30 s before the current time  
    if time > 30
         x_lim_min = time - 30;
         x_lim_max = time;
         xlim([x_lim_min x_lim_max]) 
    end
-    time_data(idx) = time;
-    temp_data(idx) = temp;
-    set(h, 'XData', time_data(1:idx), 'YData', temp_data(1:idx));
-    drawnow;
+    time_data(idx) = time; % store the current time to the time array
+    temp_data(idx) = temp; % store the current temperature to the temperature array
+    set(h, 'XData', time_data(1:idx), 'YData', temp_data(1:idx)); % set the value of the empty figure, X is the time value, Y is the temperature value
+    drawnow; % update the figure
 
     if temp < 18
+        % the first condition (temperature values are lower than 18 C)
         writeDigitalPin(a, 'D3', 0);  
         writeDigitalPin(a, 'D7', 0);  
-        writeDigitalPin(a, 'D5', 1); 
-        pause(0.5);
-        writeDigitalPin(a, 'D5', 0);  
-        pause(0.5);
+        writeDigitalPin(a, 'D5', 1); % turn on the yellow LED
+        pause(0.5); % set the interval to be 0.5
+        writeDigitalPin(a, 'D5', 0); % turn off the yellow LED
+        pause(0.5); % set the interval to be 0.5
     elseif temp > 24
+        % the second condition (temperature values are greater than 24 C)
         writeDigitalPin(a, 'D3', 0);  
         writeDigitalPin(a, 'D5', 0);  
-        writeDigitalPin(a, 'D7', 1); 
-        pause(0.25);
-        writeDigitalPin(a, 'D7', 0); 
-        pause(0.25);
+        writeDigitalPin(a, 'D7', 1); % turn on the red LED
+        pause(0.25); % set the interval to be 0.25
+        writeDigitalPin(a, 'D7', 0); % turn off the yellow LED
+        pause(0.25); % set the interval to be 0.25
     else
-        writeDigitalPin(a, 'D3', 1); 
+        % the third condition (temperature values ranges from 18 to 24 C)
+        writeDigitalPin(a, 'D3', 1); % turn on the green LED constantly
         writeDigitalPin(a, 'D5', 0); 
         writeDigitalPin(a, 'D7', 0);
     end
     
-    idx = idx + 1;
-    if idx > 30
+    idx = idx + 1; % update the index of the data, and store the next data 
+    if idx > 30 % if the data is more than 30 points, cover the stored data and start from 1
         idx = 1;
     end
-    pause_time = toc - time;
+    %set the sampling interval to be 1 second
+    pause_time = toc - time; % if the time consumed to run the code smaller than one and make a pause
     if pause_time < 1
         pause(1 - pause_time);
     end
